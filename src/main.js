308f4ec5539149db4011fd18ac2185f726fd580b
William Milanez <william.milanez@outlook.com>
2026-01-05 21:54:42 -0300

    incluindo o CSS na configuração do Webpack para carregar a estilização do projeto

---
+++ src/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// CSS
+import "./styles/form.css";
+import "./styles/global.css";
+import "./styles/schedule.css";
--- webpack.config.js
+++ webpack.config.js
@@ -26,4 +26,13 @@ module.exports = {
       favicon: path.resolve("src", "assets", "scissors.svg"),
     }),
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
