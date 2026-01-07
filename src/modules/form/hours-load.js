01043be30e258456b163117c294e5a92ae5f66bd
William Milanez <william.milanez@outlook.com>
2026-01-06 22:24:40 -0300

    carregando os horários utilizando funções para carregar horários disponíveis e fazendo a desestruturação de dados além da verificação de horários passados, tudo dentro de uma pasta específica com um arquivo de carregamento dos agendamentos do dia

---
+++ src/modules/form/hours-load.js
@@ -0,0 +1,19 @@
+import dayjs from "dayjs";
+import { openingHours } from "../../utils/opening-hours.js";
+
+export function hoursLoad({ date }) {
+  const opening = openingHours.map((hour) => {
+    // Recupera somente a hora.
+    const [schedulesHour] = hour.split(":");
+
+    // Adiciona a hora na data e verifica se está no passado.
+    const isHourPast = dayjs(date).add(schedulesHour, "hour").isAfter(dayjs());
+
+    return {
+      hour,
+      available: isHourPast,
+    };
+  });
+
+  console.log(opening);
+}
