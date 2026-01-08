d44b2d98854a7bac940796b9c967b26c1b53dac7
William Milanez <william.milanez@outlook.com>
2026-01-08 18:45:28 -0300

    enviando um horário selecionado para a API, validando campos obrigatórios para serem preenchidos corretamente evitando erros e inserindo um identificador único para o agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector("form");
+const clientName = document.getElementById("client");
 const selectedDate = document.getElementById("date");
 
 // Data atual para formatar o input.
@@ -14,5 +15,37 @@ form.onsubmit = (event) => {
   // Previne o comportamento padrão de carregar a página.
   event.preventDefault();
 
-  console.log("Formulário enviado!");
+  try {
+    // Recuperando o nome do cliente.
+    const name = clientName.value.trim();
+
+    if (!name) {
+      return alert("Informe o nome do cliente!");
+    }
+
+    // Recupera o horário selecionado.
+    const hourSelected = document.querySelector(".hour-selected");
+
+    if (!hourSelected) {
+      return alert("Selecione a hora!");
+    }
+
+    // Recupera somente a hora.
+    const [hour] = hourSelected.innerText.split(":");
+
+    // Insere a hora na data.
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+
+    // Gera um ID
+    const id = new Date().getTime();
+
+    console.log({
+      id,
+      name,
+      when,
+    });
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento.");
+    console.log(error);
+  }
 };
