73a3f063efbf155f05907aac6a31b9a4a1de4b4d
William Milanez <william.milanez@outlook.com>
2026-01-12 21:25:43 -0300

    carregando horários para outros dias, atualizando automaticamente os horários disponíveis com base na data selecionada, foi criado o arquivo date-change.js para lidar com essa lógica, além disto, foi implementado um código para recriar a lista de horários disponíveis ao mudar a data, garantindo que os horários sejam atualizados corretamente.

--- src/main.js
+++ src/main.js
@@ -9,5 +9,7 @@ import "./styles/global.css";
 import "./styles/schedule.css";
 
 // JS
+import "./modules/form/date-change.js";
 import "./modules/form/submit.js";
+
 import "./modules/page-load.js";
