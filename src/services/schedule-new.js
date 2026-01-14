9dba2b0678ca389afe40a7613eb2e73d04bd3eaf
William Milanez <william.milanez@outlook.com>
2026-01-13 21:59:49 -0300

    criando um novo arquivo para buscar e listar agendamentos do dia na aplicação

--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -2,6 +2,7 @@ import { apiConfig } from "./api-config.js";
 
 export async function scheduleNew({ id, name, when }) {
   try {
+    // Faz a requisição para enviar os dados do agendamento.
     await fetch(`${apiConfig.baseURL}/schedules`, {
       method: "POST",
       headers: {
@@ -10,6 +11,7 @@ export async function scheduleNew({ id, name, when }) {
       body: JSON.stringify({ id, name, when }),
     });
 
+    // Exibe mensagem de agendamento realizado.
     alert("Agendamento realizado com sucesso!");
   } catch (error) {
     console.log(error);
