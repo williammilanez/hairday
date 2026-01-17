7a8765980eb378fbac3e2d88d05113dcb4fd3be7
William Milanez <william.milanez@outlook.com>
2026-01-16 22:06:07 -0300

    renderizando os agendamentos da API em períodos do dia no console, criando um método para exibir os agendamentos, percorrer a lista de agendamentos e adicionar elementos HTML dinamicamente, e por fim, organizando o código e utilizar o try catch para lidar com erros

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,5 +1,6 @@
 import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
+import { schedulesShow } from "../schedules/show.js";
 
 // Seleciona o input de data.
 const selectedDate = document.getElementById("date");
@@ -10,7 +11,9 @@ export async function schedulesDay() {
 
   // Busca na API os agendamentos.
   const dailySchedules = await scheduleFetchByDay({ date });
-  console.log(dailySchedules);
+
+  // Exibe os agendamentos
+  schedulesShow({ dailySchedules });
 
   // Renderiza as horas disponíveis.
   hoursLoad({ date });
