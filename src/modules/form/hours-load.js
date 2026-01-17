3c3f97e43c3db694a5f21d74abce7405183721bf
William Milanez <william.milanez@outlook.com>
2026-01-17 14:00:50 -0300

    bloquando horários já agendados utilizando a função includes para verificar se o horário está na lista de horários ocupados e a função isBefore para verificar se o horário já pasou

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -4,20 +4,27 @@ import { hoursClick } from "./hours-click.js";
 
 const hours = document.getElementById("hours");
 
-export function hoursLoad({ date }) {
+export function hoursLoad({ date, dailySchedules }) {
   // Limpa a lista de horários.
   hours.innerHTML = "";
+
+  // Obtém a lista de horários ocupados.
+  const unavailableHours = dailySchedules.map((schedule) =>
+    dayjs(schedule.when).format("HH:mm"),
+  );
 
   const opening = openingHours.map((hour) => {
     // Recupera somente a hora.
     const [schedulesHour] = hour.split(":");
 
     // Adiciona a hora na data e verifica se está no passado.
-    const isHourPast = dayjs(date).add(schedulesHour, "hour").isAfter(dayjs());
+    const isHourPast = dayjs(date).add(schedulesHour, "hour").isBefore(dayjs());
+
+    const available = !unavailableHours.includes(hour) && !isHourPast;
 
     return {
       hour,
-      available: isHourPast,
+      available,
     };
   });
 
--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -16,5 +16,5 @@ export async function schedulesDay() {
   schedulesShow({ dailySchedules });
 
   // Renderiza as horas disponíveis.
-  hoursLoad({ date });
+  hoursLoad({ date, dailySchedules });
 }
